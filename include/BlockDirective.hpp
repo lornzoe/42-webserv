534e5246742537113d588ca6a45d2e40d2d4025b
Lorenzo Yanga <lorenzoyanga@gmail.com>
2026-07-12 00:44:13 +0800

    blockdirectives (incomplete)

--- include/BlockDirective.hpp
+++ include/BlockDirective.hpp
@@ -1,33 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   SimpleDirective.hpp                                :+:      :+:    :+:   */
+/*   BlockDirective.hpp                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lyanga <lyanga@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/11 22:13:59 by lyanga            #+#    #+#             */
-/*   Updated: 2026/07/11 23:15:21 by lyanga           ###   ########.fr       */
+/*   Created: 2026/07/11 23:12:29 by lyanga            #+#    #+#             */
+/*   Updated: 2026/07/11 23:16:39 by lyanga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SIMPLEDIRECTIVE_HPP
-#define SIMPLEDIRECTIVE_HPP
+#ifndef BLOCKDIRECTIVE_HPP
+#define BLOCKDIRECTIVE_HPP
 
 #include "Directive.hpp"
-#include <string>
-#include <vector>
 
-class SimpleDirective : public Directive 
+class BlockDirective : public Directive
 {
-	protected:
-		std::string raw_arg; // debug
 	public:
-		SimpleDirective();
-		SimpleDirective(char *raw_arg);
-		SimpleDirective(std::string raw_arg);
-		virtual ~SimpleDirective();
-
+		BlockDirective();
+		virtual ~BlockDirective();	
+		
 		virtual void print(int depth) const = 0;
-};
 
+};
 #endif
--- src/SimpleDirective.cpp
+++ src/SimpleDirective.cpp
@@ -6,10 +6,16 @@
 /*   By: lyanga <lyanga@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/11 22:25:23 by lyanga            #+#    #+#             */
-/*   Updated: 2026/07/11 22:25:33 by lyanga           ###   ########.fr       */
+/*   Updated: 2026/07/11 23:10:57 by lyanga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "SimpleDirective.hpp"
+
+SimpleDirective::SimpleDirective() {}
+
+SimpleDirective::SimpleDirective(char *raw_arg): raw_arg(raw_arg) {}
+
+SimpleDirective::SimpleDirective(std::string raw_arg): raw_arg(raw_arg) {}
 
 SimpleDirective::~SimpleDirective() {}
