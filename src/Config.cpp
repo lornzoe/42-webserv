cd306010642481b6fa8346919f8badde7dd5c19c
lorenzo yanga <lorenzoyanga@gmail.com>
2026-07-14 17:19:50 +0800

    til unnamed namespaces were intended to replace static keywords in functions

--- src/Config.cpp
+++ src/Config.cpp
@@ -6,7 +6,7 @@
 /*   By: lyanga <lyanga@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/14 07:09:40 by lyanga            #+#    #+#             */
-/*   Updated: 2026/07/14 07:41:53 by lyanga           ###   ########.fr       */
+/*   Updated: 2026/07/14 17:18:00 by lyanga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,205 +15,207 @@
 #include <sstream>
 #include <fstream>
 
-static std::vector<std::string> loadFile(char *file)
-{
-	std::ifstream infile(file); 
-	if (!infile) { 
-        std::cerr << "Error: Could not open the file!" << std::endl;
-        throw std::exception(); // replace this with a better exception
-    }
-
-    std::string line;
-	std::vector<std::string> lines;
-    while (std::getline(infile, line)) {
-		lines.push_back(line);
-    }
-    infile.close();
-
-    return lines;
-}
-
-static bool checkBraces(std::vector<std::string> lines)
-{
-	bool single_quotes = false;
-	bool double_quotes = false;
-    int indent = 0;
-    for (std::vector<std::string>::iterator it = lines.begin(); it != lines.end(); ++it)
+namespace {
+	std::vector<std::string> loadFile(char *file)
 	{
-        for (std::string::const_iterator cit = it->begin(); cit != it->end(); ++cit)
-		{
-			if (*cit == '\'')
-			{
-				if (!double_quotes)
-					single_quotes = !single_quotes;
-			}	
-			else if (*cit == '"')
-			{
-				if (!single_quotes)
-					double_quotes = !double_quotes;
-			}
-			else if (*cit == '{')
-			{
-				if (single_quotes || double_quotes)
-						continue;
-				indent++;
-			}
-            else if (*cit == '}')
-            {
-                if (single_quotes || double_quotes)
-                    continue;
-                if (indent == 0)
-                    return false;
-				indent--;
-            }
+		std::ifstream infile(file); 
+		if (!infile) { 
+			std::cerr << "Error: Could not open the file!" << std::endl;
+			throw std::exception(); // replace this with a better exception
 		}
-    }
-    return true;
-}
 
-static void stripComments(std::vector<std::string>& lines)
-{
-	bool single_quotes = false;
-	bool double_quotes = false;
-	for (std::vector<std::string>::iterator it = lines.begin(); it != lines.end(); ++it)
-	{
-		std::size_t pos = std::string::npos;
-		std::size_t idx = 0;
-		for (std::string::const_iterator cit = it->begin(); cit != it->end(); ++cit)
-		{
-			if (*cit == '\'')
-			{
-				if (!double_quotes)
-					single_quotes = !single_quotes;
-			}	
-			else if (*cit == '"')
-			{
-				if (!single_quotes)
-					double_quotes = !double_quotes;
-			}
-			if (*cit == '#')
-			{
-				if (single_quotes || double_quotes)
-						continue;
-				pos = idx;
-				break;
-			}
+		std::string line;
+		std::vector<std::string> lines;
+		while (std::getline(infile, line)) {
+			lines.push_back(line);
 		}
-		if (pos != std::string::npos)
-			it->erase(pos);
+		infile.close();
+
+		return lines;
 	}
-}
 
-static void stripEmptyLines(std::vector<std::string>& lines)
-{
-	bool single_quotes = false;
-	bool double_quotes = false;
-    for (std::vector<std::string>::iterator it = lines.begin(); it != lines.end(); )
+	bool checkBraces(std::vector<std::string> lines)
 	{
-		bool is_empty = true;
-		for (std::string::const_iterator cit = it->begin(); cit != it->end(); ++cit)
+		bool single_quotes = false;
+		bool double_quotes = false;
+		int indent = 0;
+		for (std::vector<std::string>::iterator it = lines.begin(); it != lines.end(); ++it)
 		{
-			if (*cit == '\'')
+			for (std::string::const_iterator cit = it->begin(); cit != it->end(); ++cit)
 			{
-				if (!double_quotes)
-					single_quotes = !single_quotes;
-			}	
-			else if (*cit == '"')
-			{
-				if (!single_quotes)
-					double_quotes = !double_quotes;
-			}
-			if (!std::isspace(static_cast<unsigned char>(*cit)) || single_quotes || double_quotes)
-				is_empty = false;
-		}
-		if (is_empty)
-			it = lines.erase(it);
-		else
-			++it;
-	}
-}
-
-static std::vector<std::vector<std::string> > generateTokenisedDirectives(const std::vector<std::string>& lines)
-{
-    std::vector<std::vector<std::string> > tokenised_lines;
-	bool single_quotes = false;
-	bool double_quotes = false;
-    for (std::vector<std::string>::const_iterator it = lines.begin(); it != lines.end(); ++it)
-	{
-		std::vector<std::string> tokenised_line;
-		std::string current_token;
-		// iterate by the character
-		for (std::string::const_iterator cit = it->begin(); cit != it->end(); ++cit)
-		{
-			char c = *cit;
-			if (c == '\'')
-			{
-				if (!double_quotes)
-					single_quotes = !single_quotes;
-				current_token += c;
-			}
-			else if (c == '"')
-			{
-				if (!single_quotes)
-					double_quotes = !double_quotes;
-				current_token += c;
-			}
-			else if (single_quotes || double_quotes) // everything stays together in quotes
-			{
-				current_token += c;
-			}
-			else if (std::isspace(static_cast<unsigned char>(c))) 
-			{
-				// if whitespace, push token and clear current
-				// keep the 2 conditions seperate so that multiple whitespace in a row get caught
-				if (!current_token.empty())
+				if (*cit == '\'')
 				{
-					tokenised_line.push_back(current_token);
-					current_token.clear();
+					if (!double_quotes)
+						single_quotes = !single_quotes;
+				}	
+				else if (*cit == '"')
+				{
+					if (!single_quotes)
+						double_quotes = !double_quotes;
+				}
+				else if (*cit == '{')
+				{
+					if (single_quotes || double_quotes)
+							continue;
+					indent++;
+				}
+				else if (*cit == '}')
+				{
+					if (single_quotes || double_quotes)
+						continue;
+					if (indent == 0)
+						return false;
+					indent--;
 				}
 			}
-			else if (c == '{' || c == '}' || c == ';')
+		}
+		return true;
+	}
+
+	void stripComments(std::vector<std::string>& lines)
+	{
+		bool single_quotes = false;
+		bool double_quotes = false;
+		for (std::vector<std::string>::iterator it = lines.begin(); it != lines.end(); ++it)
+		{
+			std::size_t pos = std::string::npos;
+			std::size_t idx = 0;
+			for (std::string::const_iterator cit = it->begin(); cit != it->end(); ++cit)
 			{
-				if (!current_token.empty())
+				if (*cit == '\'')
 				{
-					tokenised_line.push_back(current_token);
-					current_token.clear();
+					if (!double_quotes)
+						single_quotes = !single_quotes;
+				}	
+				else if (*cit == '"')
+				{
+					if (!single_quotes)
+						double_quotes = !double_quotes;
 				}
-				std::string s(1, c); // save it as its own string
-				tokenised_line.push_back(s);
+				if (*cit == '#')
+				{
+					if (single_quotes || double_quotes)
+							continue;
+					pos = idx;
+					break;
+				}
 			}
+			if (pos != std::string::npos)
+				it->erase(pos);
+		}
+	}
+
+	void stripEmptyLines(std::vector<std::string>& lines)
+	{
+		bool single_quotes = false;
+		bool double_quotes = false;
+		for (std::vector<std::string>::iterator it = lines.begin(); it != lines.end(); )
+		{
+			bool is_empty = true;
+			for (std::string::const_iterator cit = it->begin(); cit != it->end(); ++cit)
+			{
+				if (*cit == '\'')
+				{
+					if (!double_quotes)
+						single_quotes = !single_quotes;
+				}	
+				else if (*cit == '"')
+				{
+					if (!single_quotes)
+						double_quotes = !double_quotes;
+				}
+				if (!std::isspace(static_cast<unsigned char>(*cit)) || single_quotes || double_quotes)
+					is_empty = false;
+			}
+			if (is_empty)
+				it = lines.erase(it);
 			else
-				current_token += c;
+				++it;
 		}
-		if (!current_token.empty())
-			tokenised_line.push_back(current_token);
-		if(!tokenised_line.empty())
-			tokenised_lines.push_back(tokenised_line);
 	}
 
-    std::vector<std::vector<std::string> > directives;
-	std::vector<std::string> current_directive;
-	
-	for (std::size_t i = 0; i < tokenised_lines.size(); ++i)
+	std::vector<std::vector<std::string> > generateTokenisedDirectives(const std::vector<std::string>& lines)
 	{
-		for (std::size_t j = 0; j < tokenised_lines[i].size(); ++j)
+		std::vector<std::vector<std::string> > tokenised_lines;
+		bool single_quotes = false;
+		bool double_quotes = false;
+		for (std::vector<std::string>::const_iterator it = lines.begin(); it != lines.end(); ++it)
 		{
-			std::string token = tokenised_lines[i][j];
-			current_directive.push_back(token);
-			
-
-			if (token == ";" || token == "{" || token == "}")
+			std::vector<std::string> tokenised_line;
+			std::string current_token;
+			// iterate by the character
+			for (std::string::const_iterator cit = it->begin(); cit != it->end(); ++cit)
 			{
-				directives.push_back(current_directive);
-				current_directive.clear();
+				char c = *cit;
+				if (c == '\'')
+				{
+					if (!double_quotes)
+						single_quotes = !single_quotes;
+					current_token += c;
+				}
+				else if (c == '"')
+				{
+					if (!single_quotes)
+						double_quotes = !double_quotes;
+					current_token += c;
+				}
+				else if (single_quotes || double_quotes) // everything stays together in quotes
+				{
+					current_token += c;
+				}
+				else if (std::isspace(static_cast<unsigned char>(c))) 
+				{
+					// if whitespace, push token and clear current
+					// keep the 2 conditions seperate so that multiple whitespace in a row get caught
+					if (!current_token.empty())
+					{
+						tokenised_line.push_back(current_token);
+						current_token.clear();
+					}
+				}
+				else if (c == '{' || c == '}' || c == ';')
+				{
+					if (!current_token.empty())
+					{
+						tokenised_line.push_back(current_token);
+						current_token.clear();
+					}
+					std::string s(1, c); // save it as its own string
+					tokenised_line.push_back(s);
+				}
+				else
+					current_token += c;
+			}
+			if (!current_token.empty())
+				tokenised_line.push_back(current_token);
+			if(!tokenised_line.empty())
+				tokenised_lines.push_back(tokenised_line);
+		}
+
+		std::vector<std::vector<std::string> > directives;
+		std::vector<std::string> current_directive;
+		
+		for (std::size_t i = 0; i < tokenised_lines.size(); ++i)
+		{
+			for (std::size_t j = 0; j < tokenised_lines[i].size(); ++j)
+			{
+				std::string token = tokenised_lines[i][j];
+				current_directive.push_back(token);
+				
+
+				if (token == ";" || token == "{" || token == "}")
+				{
+					directives.push_back(current_directive);
+					current_directive.clear();
+				}
 			}
 		}
+		if (!current_directive.empty())
+		{
+			directives.push_back(current_directive);
+		}
+		return directives;
 	}
-	if (!current_directive.empty())
-	{
-		directives.push_back(current_directive);
-	}
-    return directives;
 }
 
 Config::Config(char *infile)
