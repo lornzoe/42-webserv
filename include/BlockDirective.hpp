a8319c1522522d0cc3a68b7617a93d9137d2506c
Lorenzo Yanga <lorenzoyanga@gmail.com>
2026-08-01 19:21:49 +0800

    getChildren and getChild template funcs

--- include/BlockDirective.hpp
+++ include/BlockDirective.hpp
@@ -6,7 +6,7 @@
 /*   By: lyanga <lyanga@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/11 23:12:29 by lyanga            #+#    #+#             */
-/*   Updated: 2026/07/22 17:14:42 by lyanga           ###   ########.fr       */
+/*   Updated: 2026/08/01 19:15:12 by lyanga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include "Directive.hpp"
 #include <string>
 #include <vector>
-#include <list>
+#include <cstddef>
 
 class BlockDirective : public Directive
 {
@@ -25,9 +25,34 @@ class BlockDirective : public Directive
 	public:
 		BlockDirective(TokenisedBlock::const_iterator& cit);
 		virtual ~BlockDirective();	
-		
 		virtual void print(int depth) const = 0;
+		const std::vector<Directive *>& getChildren() const;
 
-			const std::vector<Directive *>& getChildren() const;
+	protected:
+		template <typename T>
+		std::vector<const T *> getChildren() const
+		{
+			std::vector<const T *> result;
+			for (std::size_t i = 0; i < directives.size(); i++)
+			{
+				const T* match = dynamic_cast<const T *>(directives[i]);
+				if (match)
+					result.push_back(match);
+			}
+			return result;
+		}
+
+		template <typename T>
+		const T* getChild() const
+		{
+			for (std::size_t i = 0; i < directives.size(); i++)
+			{
+				const T* match = dynamic_cast<const T *>(directives[i]);
+				if (match)
+					return match;
+			}
+			return NULL;
+		}
 };
+
 #endif
