c29cd0b637c71ca537cf8a85a1fc917db00e32fb
Yong Hao Pua <ypua@c1r2s3.42singapore.sg>
2026-08-04 20:11:48 +0800

    able to parse GET request

--- include/HttpRequest.hpp
+++ include/HttpRequest.hpp
@@ -6,7 +6,7 @@
 /*   By: ypua <ypua@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/03 19:37:55 by ypua              #+#    #+#             */
-/*   Updated: 2026/08/03 20:29:55 by ypua             ###   ########.fr       */
+/*   Updated: 2026/08/04 20:07:00 by ypua             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <cstddef>
 #include <iostream>
 #include <map>
+#include <set>
 #include <sstream>
 #include <stdexcept>
 #include <string>
@@ -30,6 +31,8 @@ public:
 	HttpRequest(std::string);
 	std::string get_method();
 	std::string get_path();
+	std::string get_http_version();
+	void show_all();
 
 private:
 	std::string method_;
@@ -37,10 +40,11 @@ private:
 	std::string http_version_;
 	// std::string query_string;
 
-	std::map<std::string, std::string> headers;
+	std::set<std::string> header_keys;
+	std::map<std::string, std::string> headers_;
 
 	// Body (ignored for GET, populated for POST)
-	// std::string body;
+	std::string body_;
 
 	// bool is_complete;
 	// bool is_chunked;
--- src/HttpRequest.cpp
+++ src/HttpRequest.cpp
@@ -6,36 +6,52 @@
 /*   By: ypua <ypua@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/03 19:37:57 by ypua              #+#    #+#             */
-/*   Updated: 2026/08/03 20:30:04 by ypua             ###   ########.fr       */
+/*   Updated: 2026/08/04 20:09:30 by ypua             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HttpRequest.hpp"
 
+// Example
+// GET / HTTP/1.1
+// Accept-Encoding: gzip, deflate, br, zstd
+// Accept-Language: en-US,en;q=0.5
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+// Connection: keep-alive
+// Host: localhost:8080
+// Priority: u=0, i
+// Sec-Fetch-Dest: document
+// Sec-Fetch-Mode: navigate
+// Sec-Fetch-Site: none
+// Sec-Fetch-User: ?1
+// Upgrade-Insecure-Requests: 1
+// User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:142.0) Gecko/20100101 Firefox/142.0
 
-// void LeftTrim(std::string &s)
-// {
-// 	size_t found = s.find_first_not_of(SPACES);
-// 	if (found != std::string::npos)
-// 		s.erase(0, found);
-// 	else
-// 		s.clear();
-// }
 
-// void RightTrim(std::string &s)
-// {
-// 	size_t found = s.find_last_not_of(SPACES);
-// 	if (found != std::string::npos)
-// 		s.erase(found + 1);
-// 	else
-// 		s.clear();
-// }
+void LeftTrim(std::string &s)
+{
+	size_t found = s.find_first_not_of(SPACES);
+	if (found != std::string::npos)
+		s.erase(0, found);
+	else
+		s.clear();
+}
 
-// void Trim(std::string &s)
-// {
-// 	LeftTrim(s);
-// 	RightTrim(s);
-// }
+void RightTrim(std::string &s)
+{
+	size_t found = s.find_last_not_of(SPACES);
+	if (found != std::string::npos)
+		s.erase(found + 1);
+	else
+		s.clear();
+}
+
+std::string Trim(std::string s)
+{
+	LeftTrim(s);
+	RightTrim(s);
+	return s;
+}
 
 HttpRequest::HttpRequest(std::string request)
 {
@@ -50,10 +66,22 @@ HttpRequest::HttpRequest(std::string request)
 	}
 
 	// Parse headers
-	// while (std::getline(stream, line))
-	// 	std::cout << line << std::endl;
+	while (std::getline(stream, line))
+	{
+		size_t pos = line.find(':');
+		if (pos != std::string::npos)
+		{
+			std::string key = Trim(line.substr(0, pos));
+			std::string value = Trim(line.substr(pos + 1));
 
-	// Read body
+			header_keys.insert(key);
+			headers_[key] = value;
+		}
+	}
+
+	// Read bodys
+	body_ = std::string(std::istreambuf_iterator<char>(stream),
+						std::istreambuf_iterator<char>());
 }
 
 std::string HttpRequest::get_method()
@@ -70,4 +98,26 @@ std::string HttpRequest::get_path()
 		throw std::runtime_error("HTTP path not set");
 
 	return path_;
+}
+
+std::string HttpRequest::get_http_version()
+{
+	if (http_version_.empty())
+		throw std::runtime_error("HTTP version not set");
+
+	return http_version_;
+}
+
+void HttpRequest::show_all()
+{
+	std::cout << get_method() << " "
+			  << get_path() << " "
+			  << get_http_version() << std::endl;
+
+	for (std::set<std::string>::iterator itr = header_keys.begin();
+		 itr != header_keys.end();
+		 ++itr)
+		std::cout << *itr << ": " << headers_.at(*itr) << std::endl;
+	
+	std::cout << body_ << std::endl;
 }
