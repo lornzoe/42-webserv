58c11a8762be05fe6802f895a6b06ec801ad0b3e
lorenzo yanga <lorenzoyanga@gmail.com>
2026-09-12 01:18:47 +0800

    Refactor autoindex handling in ReqProc and implement buildAutoindex method in HttpResponse

--- src/http/HttpResponse.cpp
+++ src/http/HttpResponse.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   HttpResponse.cpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lyanga <lyanga@student.42singapore.sg>     +#+  +:+       +#+        */
+/*   By: nzoe <nzoe@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/20 17:50:51 by lyanga            #+#    #+#             */
-/*   Updated: 2026/09/10 10:19:56 by lyanga           ###   ########.fr       */
+/*   Updated: 2026/09/12 00:31:00 by nzoe             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,15 @@
 #include "ServerDirective.hpp"
 #include "MimeTypes.hpp"
 #include "Utils.hpp"
+#include "w_logger.hpp"
 
 #include <sstream>
 #include <string>
+#include <vector>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <ctime>
+#include <iostream>
 
 /** Full NON-ERROR http response, provided the relevant parts
  * contentType -- use getContentType(resource_path) to pass appropriate MIME type
@@ -70,7 +75,7 @@ static bool		getErrorPage(ServerDirective const *servDir, const std::string &uri
 }
 
 // Builds a response for `code` using a custom error page when configured,
-// otherwise a generated default error body.
+// otherwise a generated default error body.s
 std::string		HttpResponse::buildError(int code, const std::string &uri,
 											ServerDirective const *servDir)
 {
@@ -84,34 +89,97 @@ std::string		HttpResponse::buildError(int code, const std::string &uri,
 
 namespace {
 
-	std::string buildAutoindexBody(std::vector<std::pair<std::string, std::string> > entries, std::string currLoc)
+	struct AutoindexEntry
 	{
-		// top 
+		std::string	name;
+		bool		isDir;
+		off_t		size;
+		std::string	mtime;
+	};
+
+#ifdef DEBUG
+	const char *dirent_type_str(unsigned char t)
+	{
+		switch (t)
+		{
+			case DT_REG:     return "file";
+			case DT_DIR:     return "dir";
+			case DT_LNK:     return "symlink";
+			case DT_UNKNOWN: return "unknown";
+			default:         return "other";
+		}
+	}
+#endif
+
+	// nginx-style "%d-%b-%Y %H:%M" formatting of a file's last-modified time
+	std::string formatMtime(time_t mtime)
+	{
+		char buf[32];
+		struct tm *tmInfo = localtime(&mtime);
+		strftime(buf, sizeof(buf), "%d-%b-%Y %H:%M", tmInfo);
+		return std::string(buf);
+	}
+
+	std::string buildAutoindexBody(std::vector<AutoindexEntry> entries, std::string currLoc)
+	{
+		// top
 		std::stringstream ss;
 		ss << "<html><head><title>" << currLoc << "</title></head>\r\n"
-		<< "<body><center><h1>" << currLoc << "</h1></center>\r\n";
+		<< "<body><center><h1>Index of " << currLoc << "</h1></center>\r\n"
+		<< "<table><tr><th>Name</th><th>Last Modified</th><th>Size</th></tr>\r\n";
 		// middle entries
 		for (std::size_t i = 0;  i < entries.size(); ++i)
 		{
-			
+			const AutoindexEntry &e = entries[i];
+			std::string display = e.isDir ? e.name + "/" : e.name;
+
+			ss << "<tr><td><a href=\"" << display << "\">" << display << "</a></td>"
+				<< "<td>" << e.mtime << "</td>"
+				<< "<td>";
+			if (e.isDir)
+				ss << "-";
+			else
+				ss << e.size;
+			ss << "</td></tr>\r\n";
 		}
 		//bottom
-		ss << "<hr><center>webserv</center></body></html>\r\n";
+		ss << "</table><hr><center>webserv</center></body></html>\r\n";
 
-		return std::string();
+		return ss.str();
 	}
 }
 
 std::string HttpResponse::buildAutoindex(const std::string &fsDir, const std::string &uriPath, const ServerDirective *servDir)
 {
-	(void)fsDir;
-	(void)uriPath;
-	(void)servDir;
+	DIR *d = opendir(fsDir.c_str());
+	if (!d)
+		return HttpResponse::buildError(403, uriPath, servDir);
 
-	// get directory entries into strings
+	std::vector<AutoindexEntry> entries;
+	struct dirent *entry;
+	while ((entry = readdir(d)) != NULL)
+	{
+		std::string name = entry->d_name;
+		std::string entryPath = fsDir + "/" + name;
+		struct stat est;
+		if (stat(entryPath.c_str(), &est) != 0)
+			continue;
 
-	buildAutoindexBody(std::vector<std::pair<std::string, std::string> >(), std::string());
-    return std::string();
+		LOG_DEBUG("entry: " << entry->d_ino << ", " << name << ", "
+			<< entry->d_off << ", " << entry->d_reclen << ", "
+			<< dirent_type_str(entry->d_type));
+
+		AutoindexEntry ae;
+		ae.name  = name;
+		ae.isDir = S_ISDIR(est.st_mode);
+		ae.size  = est.st_size;
+		ae.mtime = formatMtime(est.st_mtime);
+		entries.push_back(ae);
+	}
+	closedir(d);
+
+	std::string body = buildAutoindexBody(entries, uriPath);
+	return HttpResponse::build(200, "text/html", body);
 }
 
 // Generates error body if no error html resource to serve
